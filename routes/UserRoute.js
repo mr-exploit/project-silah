120fa861ef733344fc99d37f864acc412a58aaca
mr-exploit <raffiaj8@gmail.com>
2022-12-21 15:33:19 +0700

    fix bugs host value

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -12,9 +12,9 @@ import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/users',  getUsers);
+router.get('/users',  verifyUser, adminOnly, getUsers);
 router.get('/users/:id', verifyUser, adminOnly, getUserById);
-router.post('/users', createUser);
+router.post('/users', verifyUser, adminOnly,createUser);
 router.patch('/users/:id', verifyUser, adminOnly, updateUser);
 router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
 
