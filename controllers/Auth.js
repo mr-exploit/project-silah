8ce881bb947e970004ee782e78fcb08b12114376
mr-exploit <raffiaj8@gmail.com>
2022-12-25 13:29:22 +0700

    fix bugs host value

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -10,7 +10,7 @@ export const Login = async(req, res) => {
     if (!user) return res.status(404).json({ msg: "User tidak ditemukan" });
     // const match = await argon2.verify(user.password, req.body.password);
     // if (!match) return res.status(400).json({ msg: "Wrong Password" });
-    // req.session.userId = user.uuid;
+    req.session.userId = user.uuid;
     const uuid = user.uuid;
     const name = user.name;
     const email = user.email;
@@ -19,14 +19,14 @@ export const Login = async(req, res) => {
 }
 
 export const Me = async(req, res) => {
-    // if (!req.session.userId) {
-    //     return res.status(401).json({ msg: "Mohon Login ke akun Anda!" });
-    // }
+    if (!req.session.userId) {
+        return res.status(401).json({ msg: "Mohon Login ke akun Anda!" });
+    }
     const user = await User.findOne({
         attributes: ['uuid', 'name', 'email', 'role'],
-        // where: {
-        //     uuid: req.session.userId
-        // }
+        where: {
+            uuid: req.session.userId
+        }
     });
     if (!user) return res.status(404).json({ msg: "User tidak ditemukan" });
     res.status(200).json(user);
