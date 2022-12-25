540310ca366782d5b9b1e07b5f7109bec12138ee
mr-exploit <raffiaj8@gmail.com>
2022-12-25 18:15:41 +0700

    fix bugs host value

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-import argon2 from "argon2";
+const argon2 = require("argon2");
 
 export const Login = async(req, res) => {
     const user = await User.findOne({
